586370602625ec726144befe4a792d08aa1adcb4
Jitendra Kumar <jitendra0202006@gmail.com>
2026-06-22 17:17:05 +0530

    to ensure the account safety

--- backend/src/controllers/account.controller.js
+++ backend/src/controllers/account.controller.js
@@ -1,5 +1,15 @@
 const accountModel = require('../models/account.model.js');
 
-export async function createAccountController(req, res){
-    
+async function createAccountController(req, res){
+    const user = req.user;
+
+    const account = await accountModel.create({
+        user: user._id
+    })
+
+    return res.status(201).json({
+        account
+    })
 }
+
+module.exports = {createAccountController};
